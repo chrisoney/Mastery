ba1442089f5864f726d3ed7a2bd813d90e6113bd
chrisoney <christopher.oney@yahoo.com>
2022-03-09 15:42:51 -0800

    update footer

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -4,8 +4,8 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer-field">
-        <span className="footer-child">Copyright &copy; 2020 Mastery</span>
-        <a href="https://github.com/codedbyq/Mastery/wiki" className="footer-child">Github</a>
+        <span className="footer-child">2022 Mastery</span>
+        <a href="https://github.com/chrisoney/Mastery" className="footer-child">Github</a>
       </div>
     );
   }
